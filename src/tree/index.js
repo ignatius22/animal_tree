4d7e5c3d893d08938e6da9074fed4f880ec6ae43
ignatius22 <igee54077@gmail.com>
2022-12-27 21:08:53 +0100

    Feature: add styles and data model hierachy

--- src/tree/index.js
+++ src/tree/index.js
@@ -1,16 +1,40 @@
-import React, {useState, useEffect} from 'react';
-import './index.css'
+import React, { useState, useEffect } from "react";
+import "./index.css";
+import animalTree from "./data.json";
 
 export default function Tree() {
-    return (
-            <div className="tree">
-                mammals<br/>
+  // const parsed = JSON.parse(animalTree)
+  console.log(animalTree, "tree");
+  return (
+    <div className="tree">
+      {animalTree.animals.map((animal) => (
+        <>
+          <span className="space-0">{animal.name}</span>
+          {animal.family.map((animal) => (
+            <>
+              <span className="space-50">{animal.name}</span>
+
+              {animal.family.map((animal) => (
+                <>
+                  <span className="space-95">{animal.name}</span>
+                  {animal.family.map((animal) => (
+                    <>
+                      <span className="space-145">{animal.name}</span>
+                    </>
+                  ))}
+                </>
+              ))}
+            </>
+          ))}
+        </>
+      ))}
+      {/* mammals<br/>
                 <span className='space-50'>cheetah</span><br/>
                 <span className='space-50'>bear</span><br/>
                 <span className='space-95'>lion</span> <br/>
                 <span className='space-95'>dog</span> <br/>
                 <span className='space-145'>elephant</span> <br/>
-                <span className='space-50'>ape</span> <br/>
-            </div>
-    )
+                <span className='space-50'>ape</span> <br/> */}
+    </div>
+  );
 }
